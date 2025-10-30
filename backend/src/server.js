eccd1b2eaa86736db446da802875fb87ec86a94a
Defence Ndzhobela <defencex2@gmail.com>
2025-10-30 22:58:54 +0200

    Your changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,15 @@
-// Server entry point
+import express from 'express'
+import { testSupabaseConnection } from './config/db.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Server is running ✅')
+})
+
+testSupabaseConnection()
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`)
+})
